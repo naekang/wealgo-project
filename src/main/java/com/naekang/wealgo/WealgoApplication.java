1c88b64796b4013ed14a9289f3d3c917d7f65a27
naekang <rlawlsgh6306@gmail.com>
2022-10-19 23:20:08 +0900

    Initial Settings

---
+++ src/main/java/com/naekang/wealgo/WealgoApplication.java
@@ -0,0 +1,13 @@
+package com.naekang.wealgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WealgoApplication.class, args);
+    }
+
+}
